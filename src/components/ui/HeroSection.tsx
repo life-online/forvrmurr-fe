0a88be6d253c4813281ac308082f7b14bb35e749
tomlytm <tomilayojesse@gmail.com>
2025-05-24 09:58:39 +0100

    added filter for prodduct and search

--- src/components/ui/HeroSection.tsx
+++ src/components/ui/HeroSection.tsx
@@ -9,7 +9,7 @@ const HeroSection: React.FC = () => {
   return (
     <section className="relative w-full text-white min-h-[70vh] md:min-h-[80vh] flex items-center justify-center py-12 md:py-0 overflow-hidden">
       {/* Background Image */}
-      <div className="absolute inset-0 bg-black/20 m-4"> {/* Fallback color while image loads */}
+      <div className="absolute inset-0 bg-black/20 m-1"> {/* Fallback color while image loads */}
         <Image
           src="/images/hero/hero_1.png"
           alt="Luxury perfume collection background"
@@ -31,7 +31,7 @@ const HeroSection: React.FC = () => {
       {/* Overlay Content Container */}
       <div className="relative z-20 max-w-7xl w-full mx-auto px-12 text-left"> 
         <div className="space-y-6 md:w-1/2"> 
-          <h1 className="text-4xl md:text-5xl lg:text-6xl font-serif font-medium tracking-wide text-[#e6c789]">
+          <h1 className="text-3xl md:text-4xl lg:text-4xl font-serif font-medium tracking-wide text-[#e6c789]">
             MEET YOUR NEXT OBSESSION
           </h1>
           <p className="text-lg md:text-xl font-light text-[#e6c789]">
